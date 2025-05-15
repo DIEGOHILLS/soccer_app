3998abe2ffc0f05ab2199a25bed63ac959a2f72c
Baba Yaga <hillsdiego33@gmail.com>
2025-05-15 22:47:51 +0200

    first commit

---
+++ src/soccer_13_3/Player.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package soccer_13_3;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Player {
+    
+    private String playerName;
+    private int goalsScored;
+    
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+    
+    public Player() {}
+    //13.3
+    public void incGoalsScored() {
+    this.goalsScored++;
+    }
+    /**
+     * @return the playerName
+     */
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    /**
+     * @param playerName the playerName to set
+     */
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    /**
+     * @return the goalsScored
+     */
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    /**
+     * @param goalsScored the goalsScored to set
+     */
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+    
+}
